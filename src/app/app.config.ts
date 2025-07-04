509878d99e6aa8f843caad5cf744c92f4de573d7
Mahmoud Mo <mmahmoud092002@gmail.com>
2025-07-05 02:24:00 +0300

    some edits

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,24 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
+export function HttpLoaderFactory(http: HttpClient) {}
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(),
-    provideRouter([]),
-    TranslateModule.forRoot({
-      defaultLanguage: 'en',
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }).providers!,
-  ],
+  providers: [provideHttpClient(), provideRouter([])],
 };
